78095222e6d47f43b8841ae55cd00b730dd56a91
Katia <katia.punter@storyshareplatform.com>
2026-06-26 12:27:19 +0100

    adds auth session and logout

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import json
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, redirect, url_for
 
 app = Flask(__name__) # instantiates the Flask application
+app.secret_key = "tem_key"
 
 def load_user():
     with open("user.json", "r") as file:
@@ -9,17 +10,27 @@ def load_user():
 
 @app.route('/')
 def home():
+    if "logged_in" not in session:
+        return redirect(url_for("login"))
     return render_template('index.html')
-    # saved_user = load_user()
-    # return saved_user["username"]
-    
 
 @app.route('/login', methods= ["GET", "POST"])
 def login():
     if request.method == "POST":
+        saved_user = load_user()
         username = request.form["username"]
-        return username
+        password = request.form["password"]
+        if username == saved_user["username"] and password == saved_user["password"]:
+            session["logged_in"] = True
+            return redirect(url_for("home"))
+        
+        return "Login Fail!"
     return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for("login"))
 
 if __name__ == '__main__':
     app.run(debug=True)
