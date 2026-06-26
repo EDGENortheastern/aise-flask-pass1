c08f5f251db551035464ab8b51f1c54a432e51ba
Katia <katia.punter@storyshareplatform.com>
2026-06-26 11:44:12 +0100

    adds route login

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
+import json
 from flask import Flask, render_template
 
 app = Flask(__name__) # instantiates the Flask application
 
+def load_user():
+    with open("user.json", "r") as file:
+        return json.load(file)
+
 @app.route('/')
 def home():
     return render_template('index.html')
+    # saved_user = load_user()
+    # return saved_user["username"]
+    
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
